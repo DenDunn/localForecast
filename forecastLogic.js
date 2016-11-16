016c0faff1815af8c5180f9170693c5ddaff6a25
Den <den@Dens-MacBook-Air.local>
2016-11-16 23:34:35 +0300

    tune icons

--- forecastLogic.js
+++ forecastLogic.js
@@ -3,8 +3,6 @@
  */
 
 /*
-* request: http://api.openweathermap.org/data/2.5/weather?lat=55.7702012&lon=37.6024752&APPID=%putYourKeyHere%
-*
 * replay format:
 * {"coord":{"lon":37.61,"lat":55.76},
 * "weather":[{"id":621,"main":"Snow","description":"shower snow","icon":"13n"}],
@@ -18,23 +16,53 @@
 * ,"id":524894,
 * "name":"Moskva",
 * "cod":200}
+* I need from here: json.weather[0].id -> draw the icon for the weather
+*                   json.weather[0].description -> sigh the icon italic
+*                   json.main.temp -> temperature
+*                   json.name -> city's name
+*                   json.sys.country -> country code
 * */
 
 $(document).ready(function(){
-    function getMyLocation () {
+    //loading icons mapping between openweathermap and weather icon provided by owm-icons.json
+    var icon_map = (function() {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'dataType': "json",
+            'url': './owm-icons.json',
+            'success': function(data) {
+                json = data;
+            }
+        });
+        return json;
+    })();
+
+    //grab location
+    var position = (function getMyLocation () {
         if (navigator.geolocation){
-            // call getCurrentPosition() and send returned data to callback function viewOnPage
-            navigator.geolocation.getCurrentPosition(viewOnPage);
+            // call getCurrentPosition() and send returned data to callback function getLocalWeather()
+            navigator.geolocation.getCurrentPosition(getLocalWeather);
         } else {
             $("#show").html("'navigator.geolocation' does not supported.");
         }
+    })();
+
+    function getLocalWeather(position){
+        $("#show").html("longitude: " + position.coords.longitude + "; latitude: " + position.coords.latitude);//debug
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather?lat='+ position.coords.latitude +'&lon='+ position.coords.longitude +'&APPID=3eb66f7f859ef68a4d68ee954a1f6c78&units=metric&lang=en',
+            cached: false,
+            dataType: 'json',
+            success: function(resp) {
+                $("#show3").html(JSON.stringify(resp.weather[0].id));
+            }
+        });
     }
 
-    function viewOnPage(position){
-        $("#show").html("longitude: " + position.coords.longitude + "; latitude: " + position.coords.latitude);
-    }
 
-    getMyLocation();
+    //debug
+    $("#show2").html(JSON.stringify(icon_map[200].icon));
 
-    var icon_map = 
 })
